2c9a688810aa3d7fedc48de1fc0c7801e8a161ad
Mariusz <rexxarhell@gmail.com>
2023-10-25 22:29:10 +0200

    Bubble algorithm added

--- src/com/solvd/laba/block1/task1b/Main.java
+++ src/com/solvd/laba/block1/task1b/Main.java
@@ -12,15 +12,20 @@ public class Main {
         //List of algorithms
         List<Sortable> sortables = List.of(
                 new InsertionSort(),
-                new SelectionSort()
+                new SelectionSort(),
+                new BubbleSort()
         );
         int[] copyOfArray;
+
+        System.out.println("Array: ");
+        ArrayCreator.printArray(array);
         for (Sortable sortable:sortables) {
             copyOfArray = Arrays.copyOf(array, array.length);
             startTime = System.currentTimeMillis();
+            System.out.println(sortable.getName() + " algorithm: ");
             ArrayCreator.printArray(sortable.sort(copyOfArray));
             endTime = System.currentTimeMillis();
-            System.out.println("Time taken by " + sortable.getName() + " algorithm to sort the array: " + (endTime - startTime) + " milliseconds");
+            System.out.println("Time taken by " + sortable.getName() + " algorithm to sort the array: " + (endTime - startTime) + " milliseconds" + "\n");
         }
         //ArrayCreator.printArray(array);
 
