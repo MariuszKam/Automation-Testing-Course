71e79df9324eec6d0c88af4dae9296b0ec498bc0
Mariusz <rexxarhell@gmail.com>
2023-10-25 19:46:39 +0200

    Implementing task1b

---
+++ src/com/solvd/laba/block1/task1b/ArrayCreator.java
@@ -0,0 +1,22 @@
+package com.solvd.laba.block1.task1b;
+
+import java.util.Random;
+
+public class ArrayCreator {
+
+    public static int[] createRandomArray(int amount) {
+        Random random = new Random();
+        int[] array = new int[amount];
+        //Feeding array with random numbers
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int num:array) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ src/com/solvd/laba/block1/task1b/Main.java
@@ -0,0 +1,9 @@
+package com.solvd.laba.block1.task1b;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = ArrayCreator.createRandomArray(10);
+        ArrayCreator.printArray(array);
+
+    }
+}
