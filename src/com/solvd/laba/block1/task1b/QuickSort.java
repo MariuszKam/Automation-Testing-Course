6a93384c5de1cf2343c53d64c27316434f4167da
Mariusz <rexxarhell@gmail.com>
2023-10-26 19:29:12 +0200

    Building up Quicksort

---
+++ src/com/solvd/laba/block1/task1b/QuickSort.java
@@ -0,0 +1,15 @@
+package com.solvd.laba.block1.task1b;
+
+public class QuickSort implements Sortable {
+
+    private final String NAME = "Quicksort";
+    @Override
+    public int[] sort(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public String getName() {
+        return this.NAME;
+    }
+}
