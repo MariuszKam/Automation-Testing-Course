6b6fee4ef0ed627ee23d32fbc2ef1122b1808d83
Mariusz <rexxarhell@gmail.com>
2023-10-30 23:00:12 +0100

    Redo task

--- src/com/solvd/laba/block1/task2/Main.java
+++ src/com/solvd/laba/block1/task2/Main.java
@@ -1,17 +1,8 @@
 package com.solvd.laba.block1.task2;
 
 
-import com.solvd.laba.block1.task2.models.Shop;
-import com.solvd.laba.block1.task2.services.ShopService;
-
 public class Main {
     public static void main(String[] args) {
-        Shop shop = new Shop();
-        ShopCreator.initializeShop(shop);
-        ShopService shopService = new ShopService(shop);
-        shopService.showStaff();
-        shopService.payroll();
-        shopService.showInventory();
 
 
     }
