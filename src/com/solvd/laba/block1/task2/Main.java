d24b8d45d6abd645787de0407a0536a4192e5417
Mariusz <rexxarhell@gmail.com>
2023-10-30 19:13:03 +0100

    Added shop initialization

--- src/com/solvd/laba/block1/task2/Main.java
+++ src/com/solvd/laba/block1/task2/Main.java
@@ -7,10 +7,11 @@ import com.solvd.laba.block1.task2.services.ShopService;
 public class Main {
     public static void main(String[] args) {
         Shop shop = new Shop();
-        ShopCreator.createStaff(shop);
+        ShopCreator.initializeShop(shop);
         ShopService shopService = new ShopService(shop);
         shopService.showStaff();
         shopService.payroll();
+        shopService.showInventory();
 
 
     }
--- src/com/solvd/laba/block1/task2/ShopCreator.java
+++ src/com/solvd/laba/block1/task2/ShopCreator.java
@@ -1,6 +1,7 @@
 package com.solvd.laba.block1.task2;
 
 import com.solvd.laba.block1.task2.models.Employee;
+import com.solvd.laba.block1.task2.models.Item;
 import com.solvd.laba.block1.task2.models.Shop;
 import com.solvd.laba.block1.task2.services.ShopService;
 
@@ -9,16 +10,17 @@ import java.util.Random;
 
 
 public class ShopCreator {
+    private static Random random = new Random();
 
 
-    public static void createStaff(Shop shop) {
+    private static void createStaff(Shop shop) {
+        //Array of names and lastnames
         String[] name = {"Amelia", "Jackson", "Olivia", "Ethan", "Sophia", "Liam", "Mia", "Noah", "Ava", "Lucas"};
         String[] lastname = {"Parker", "Bennett", "Mitchell", "Cooper", "Anderson", "Johnson", "Turner", "Collins", "Davis", "Wilson"};
         ShopService shopService = new ShopService(shop);
-        Random random = new Random();
 
         for (int i = 1; i < 12; i++) {
-            //Same length for both arrays
+            //Number for an index to pick random name and lastname from array.
             int randomName = random.nextInt(name.length);
             int randomLastname = random.nextInt(lastname.length);
             double salary = random.nextDouble(2200.00 - 800.00 + 1) + 800.00;
@@ -30,5 +32,25 @@ public class ShopCreator {
             shopService.hire(new Employee(i, name[randomName], lastname[randomLastname], salary, "Customer Service"));
         }
 
+    }
+
+    private static void createInventory(Shop shop) {
+        String[] items = {"Pencil", "Notebook", "Soccer Ball", "Teddy Bear", "Calculator", "Backpack", "Headphones",
+                "Coffee Mug", "Smartphone Case", "Sunglasses", "Umbrella", "Water Bottle", "Wallet", "Watch", "Laptop",
+                "T-shirt", "Sneakers", "Perfume", "Alarm Clock", "Kitchen Knife Set", "Yoga Mat", "Dumbbell Set",
+                "Plant Pot", "Towel Set", "Picture Frame", "Candle Holder", "Bluetooth Speaker", "Desk Lamp",
+                "Wall Clock", "Stationery Set"};
+        ShopService shopService = new ShopService(shop);
+        for (int i = 0; i < items.length; i++) {
+            int id = i + 1;
+            double price = random.nextDouble(30.00 - 5.00 + 1) + 5.00;
+            int amount = random.nextInt(100);
+            shopService.addItem(new Item(id, items[i], price, amount));
+        }
+    }
+
+    public static void initializeShop(Shop shop) {
+        createStaff(shop);
+        createInventory(shop);
     }
 }
