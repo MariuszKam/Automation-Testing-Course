5cd0533e97270bc8021b16a29334b91e195d5f88
Mariusz <rexxarhell@gmail.com>
2023-11-13 15:03:31 +0100

    Tries to implement add to LinkedList

--- src/com/solvd/laba/block1/task2/models/shop/components/MyLinkedList.java
+++ src/com/solvd/laba/block1/task2/models/shop/components/MyLinkedList.java
@@ -1,4 +1,25 @@
 package com.solvd.laba.block1.task2.models.shop.components;
 
 public class MyLinkedList<T> {
+    private Node<T> head;
+
+    private static class Node<T> {
+        private T element;
+        private Node<T> next;
+
+        public Node(T element) {
+            this.element = element;
+            this.next = null;
+        }
+    }
+
+    public void add(T element) {
+        Node<T> newNode = new Node<>(element);
+        Node<T> currentNode = head;
+
+        while (currentNode != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode = newNode;
+    }
 }
