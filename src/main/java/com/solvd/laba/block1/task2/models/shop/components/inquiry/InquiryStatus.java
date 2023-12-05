bb0a5360559183295e82ad34d0b3d9921af9a77c
Mariusz <rexxarhell@gmail.com>
2023-12-05 02:48:50 +0100

    Method in enum

--- src/main/java/com/solvd/laba/block1/task2/models/shop/components/inquiry/InquiryStatus.java
+++ src/main/java/com/solvd/laba/block1/task2/models/shop/components/inquiry/InquiryStatus.java
@@ -1,5 +1,7 @@
 package com.solvd.laba.block1.task2.models.shop.components.inquiry;
 
+import static com.solvd.laba.block1.task2.Main.logger;
+
 public enum InquiryStatus {
     IN_PROGRESS("In Progress"),
     RESOLVED("Resolved");
@@ -14,5 +16,7 @@ public enum InquiryStatus {
         return status;
     }
 
-
+    public void statusInfo() {
+        logger.info("Inquiry is {}", getStatus());
+    }
 }
